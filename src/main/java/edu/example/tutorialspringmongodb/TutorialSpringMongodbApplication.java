fb0f731fff40047ea707aa9bb9ae8aab685cfb42
Thiago Moura <thiago.moura@ccc.ufcg.edu.br>
2018-11-16 11:13:55 -0300

    first commit

---
+++ src/main/java/edu/example/tutorialspringmongodb/TutorialSpringMongodbApplication.java
@@ -0,0 +1,12 @@
+package edu.example.tutorialspringmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialSpringMongodbApplication.class, args);
+	}
+}
